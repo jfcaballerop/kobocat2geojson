62f0e832634d0e32497a828750fda658a9c08bf0
jfcp <jfcp@inesingenieros.com>
2017-09-25 12:01:06 +0200

    add request method

--- test/test.js
+++ test/test.js
@@ -1,17 +1,20 @@
 var kobo2geojson = require('../lib/kobocat2geojson');
 
 
-if (process.argv.length <= 9) {
+if (process.argv.length <= 4) {
     console.log(process.argv);
-    console.log("\n## ERROR ##\n\nUsage: node " + __filename + " path collection host port db user pass authdb");
+    // console.log("\n## ERROR ##\n\nUsage: node " + __filename + " path collection host port db user pass authdb");
+    console.log("\n## ERROR ##\n\nUsage: node " + __filename + " kobouser kobopass kobohost kobopath");
     process.exit(-1);
 }
-var path = process.argv[2];
-var collection = process.argv[3];
-var host = process.argv[4];
-var port = process.argv[5];
-var db = process.argv[6];
-var user = process.argv[7];
-var pass = process.argv[8];
-var authdb = process.argv[9];
-console.log('Resultado:: ' + kobo2geojson.parse2savemongo(path, collection, host, port, db, user, pass, authdb));
+var kobouser = process.argv[2];
+var kobopass = process.argv[3];
+var kobohost = process.argv[4];
+var kobopath = process.argv[5];
+// var host = process.argv[4];
+// var port = process.argv[5];
+// var db = process.argv[6];
+// var user = process.argv[7];
+// var pass = process.argv[8];
+// var authdb = process.argv[9];
+console.log('Resultado:\n' + kobo2geojson.connect2Kobo(kobouser, kobopass, kobohost, kobopath));
